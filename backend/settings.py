49cfc570a873ae85f1179bc9d3b476962e51bb62
AzureAD\MattiaFaraci <mattia.faraci@e38.it>
2025-10-02 20:38:49 +0200

    Rimuovi riferimenti all'immagine E38.png e aggiorna il percorso del favicon in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -42,11 +42,11 @@ class _UiSettings(BaseSettings):
     )
 
     title: str = "E38 - AI Analysis"
-    logo: Optional[str] = "/E38.png"
-    chat_logo: Optional[str] = "/E38.png"
+    logo: Optional[str] = None
+    chat_logo: Optional[str] = None
     chat_title: str = "Salve, mi dica il suo nome e il ruolo che ricopre in Cosiam S.r.l."
     chat_description: str = "Questo chatbot è configurato per analizzare le attività svolte quotidianamente ed individuare possibili applicazioni pratiche dell’AI."
-    favicon: str = "/E38.png"
+    favicon: str = "/favicon.ico"
     show_share_button: bool = False
     show_chat_history_button: bool = False
 
