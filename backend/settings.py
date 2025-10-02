9f7664727dc1cad1ee9eaa56582da15bcdd66913
AzureAD\MattiaFaraci <mattia.faraci@e38.it>
2025-10-02 17:54:09 +0200

    Implement code changes to enhance functionality and improve performance

--- backend/settings.py
+++ backend/settings.py
@@ -41,14 +41,14 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
-    logo: Optional[str] = None
-    chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
-    favicon: str = "/favicon.ico"
-    show_share_button: bool = True
-    show_chat_history_button: bool = True
+    title: str = "E38 - AI Analysis"
+    logo: Optional[str] = "../../assets/E38.png"
+    chat_logo: Optional[str] = "../../assets/E38.png"
+    chat_title: str = "Salve, mi dica il suo nome e il ruolo che ricopre in Cosiam S.r.l."
+    chat_description: str = "Questo chatbot è configurato per analizzare le attività svolte quotidianamente ed individuare possibili applicazioni pratiche dell’AI."
+    favicon: str = "../../assets/E38.png"
+    show_share_button: bool = False
+    show_chat_history_button: bool = False
 
 
 class _ChatHistorySettings(BaseSettings):
